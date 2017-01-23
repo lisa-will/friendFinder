cd06d5fa925219f49efb2ebb5541c19707e3747e
Lisa Williams <williams.lisarenee@gmail.com>
2017-01-23 16:24:42 -0500

    Debugging

--- app/routing/api-routes.js
+++ app/routing/api-routes.js
@@ -22,31 +22,3 @@ module.exports = function(app){
 		    res.json(true);
 	});
 }
-
-//      // req.body hosts is equal to the JSON post sent from the user 
-//      var newFriend = req.body;
-//      friends.push(newFriend);
-
-//      var totalDifference = 0;
-//      var differencesArray = [];
-
-//      // Loop through friends, minus one (current user)
-//      for (var i = 0; i < (friends.length - 1); i++){
-
-//          // Loop through the question (q) values 
-//          for (var q = 0; q < friends[i].score.length; q++){
-//                 totalDifference += Math.abs(friends[i].scores[q] - newFriend.scores[q]);
-//          }
-
-//          differencesArray.push(totalDifference);
-//          totalDifference = 0;
-//          }
-
-//      // Give the smallest value in the array 
-//      var bestMatch = friends[differencesArray.indexOf(Math.min.apply(null, differencesArray))];
-     
-//      res.send(bestMatch);
-
-// });
-
-// }
--- app/routing/html-routes.js
+++ app/routing/html-routes.js
@@ -4,12 +4,12 @@ module.exports = function(app){
 
     // Basic route that sends the user first to the AJAX 
     app.get('/', function(req, res){
-        res.sendFile(path.join(__dirname + '/../public/home.html'));
+        res.sendFile(path.join(__dirname, '..', 'public', 'home.html'));
     })
 
     // Sends user to survey page 
     app.get('/survey', function(req, res){
 
-        res.sendFile(path.join(__dirname + '/../public/survey.html'));
+        res.sendFile(path.join(__dirname, '..', 'public', 'survey.html'));
     })
 };
